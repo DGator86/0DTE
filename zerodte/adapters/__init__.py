fc08fa5558a3b5b9c0f6abf4ea4c9fc8edfa83a8
DGator86 <djvogeli@gmail.com>
2026-07-16 09:09:06 -0400

    refactor: add legacy compatibility adapter package

---
+++ zerodte/adapters/__init__.py
@@ -0,0 +1,5 @@
+"""Compatibility adapters used while legacy top-level modules are migrated."""
+
+from .legacy_snapshot import canonical_snapshot_from_tick
+
+__all__ = ["canonical_snapshot_from_tick"]
